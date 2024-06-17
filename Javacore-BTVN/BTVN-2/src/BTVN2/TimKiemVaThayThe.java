c6d19d13635e78714f32cb1b4eeac66d2de87456
Huongin <huongtranvn1109@gmail.com>
2024-06-17 12:23:19 +0900

    JavaBTVN2-5BTVN2-5

--- Javacore-BTVN/BTVN-2/src/BTVN2/TimKiemVaThayThe.java
+++ Javacore-BTVN/BTVN-2/src/BTVN2/TimKiemVaThayThe.java
@@ -3,17 +3,17 @@ package BTVN2;
 public class TimKiemVaThayThe {
     public static void main(String[] args) {
 
-         String S = "Cho dù bạn không sinh ra và lớn lên tại Nhật, nhưng nếu đã trót yêu nền văn hóa giàu bản sắc này, thì bạn hoàn toàn có thể thiết kế căn nhà của mình theo kiểu Nhật";
+         String S = " Cho du ban khong sinh ra va lon len o Nhat, nhung neu trot yeu nen van hoa nay ban cung co the thiet ke can nha cua minh theo phong cach Nhat";
 
          //Tìm vị trí xuất hiện đầu tiên
          System.out.println(S.indexOf("Cho"));
 
          //Tìm vị trí xuất hiện cuối cùng
-         System.out.println(S.lastIndexOf("t"));
+         System.out.println(S.lastIndexOf("Nhat"));
 
          //Thay thế xâu ký tự
-        String S1 = "Nhật";
-        String S2 = "Việt Nam";
+        String S1 = "Nhat";
+        String S2 = "Viet Nam";
         String replace = S.replace(S1,S2);
         System.out.println(replace);
     }
